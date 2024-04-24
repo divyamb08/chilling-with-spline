f49db8c2918fef897fa199e40d823e7cab6f0071
Divyam Bansal <divyam.bansal2000@gmail.com>
2024-04-23 20:25:49 -0500

    initial

---
+++ components/hero.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function hero() {
+  return (
+    <Spline scene="https://prod.spline.design/8Ui4Asz90MuOb91e/scene.splinecode" />
+  );
+}
